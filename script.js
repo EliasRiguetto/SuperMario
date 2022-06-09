961e11142a9f7aba8fc19cdfe8e7aef8ca4dffb0
Elias <eliasriguetto@gmail.com>
2022-06-09 18:13:52 -0300

    6 - Codigo de parada

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-function JumpMario(){
-    const mario = document.querySelector('.mario')
-    mario.addEventListener('click', jump)
+const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
 
+function JumpMario(){
+    ['click','keydown'].forEach((event)=>{
+        document.addEventListener(event, jump)
+    })
 }
 
 function jump(){
-    this.classList.add('jump');
+    mario.classList.add('jump');
+    console.log('oi')
     setTimeout(()=>{
-        this.classList.remove('jump')
+        mario.classList.remove('jump')
     },1000)
-
-    
 }
 
+function loop(){
+    const time = setInterval(()=>{
+
+        const pipePosition = pipe.offsetLeft
+        const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','')
+        
+        if(pipePosition <= 80 && pipePosition > 0 && marioPosition < 130){
+           pipe.style.animation = 'none';
+           pipe.style.left = `${pipePosition}px`
+           mario.style.animation = 'none';
+           mario.style.bottom = `${marioPosition}px`
+           mario.src = "imagem/game-over.png"
+           mario.style.width = '80px';
+           clearInterval(time)
+        }
+    },10)
+}
+
+loop()
 JumpMario()
 
